51283a4338e9af88743d410ba58cdfecba94ffda
myz202 <matchy1518@gmail.com>
2024-06-22 16:20:03 +0900

    カウントダウンいいかんじ

--- Jirou/Assets/Scripts/TimeLimit.cs
+++ Jirou/Assets/Scripts/TimeLimit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,25 +6,60 @@ using UnityEngine.UI;
 public class TimeLimit : MonoBehaviour
 {
     [SerializeField]public float timeLimit = 60.0f;
+    private float waitTime = 10f;
     private float time = 0;
 
     [SerializeField] string sceneName;
     public Text text;
 
+    public AudioSource startSE;
+    public AudioSource finishSE;
+
+    public GameObject kaishi;
+    public GameObject sokomade;
+
     // Start is called before the first frame update
     void Start()
     {
         time = timeLimit;
+        waitTime = 10f;
+        kaishi.SetActive(false);
+        sokomade.SetActive(false);
+        StartCoroutine(WaitStart());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator WaitStart()
     {
-        time -= Time.deltaTime;
-        text.text = ("残り ") + time.ToString("F1");
-        if(time <= 0)
+        while(waitTime > 0)
         {
-            SceneManager.LoadScene(sceneName);
+            waitTime -= Time.deltaTime;
+            text.text = ("開始まで ") + waitTime.ToString("F1");
+            yield return null;
         }
+        //startSE.Play();
+        kaishi.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        kaishi.SetActive(false);
+        StartCoroutine(Countdown());
+
+    }
+
+    private IEnumerator Countdown()
+    {
+        while(time > 0)
+        {
+            time -= Time.deltaTime;
+            text.text = ("残り ") + time.ToString("F1");
+            yield return null;
+        }
+        // 制限時間が0になったらSEを再生し、画像を表示
+        //finishSE.Play();
+        sokomade.SetActive(true);
+
+        // 2秒間待機
+        yield return new WaitForSeconds(2f);
+
+        // リザルトシーンをロード
+        SceneManager.LoadScene(sceneName);
     }
 }
